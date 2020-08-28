8d48c05b052993f409c58f3e1229413415a77ebf
Andre Vicente <isquicha@gmail.com>
2020-08-27 21:11:21 -0300

    Add create and list users functionality

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,49 @@
 import express = require("express");
+import { createUser, listUsers } from "./models/user";
 
 const app = express();
+app.use(express.json());
 app.set("port", process.env.PORT || 3000);
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.post("/createUser", (req, res) => {
+    let name: string = req.body.name;
+    let email: string = req.body.email;
+    let gender: string = req.body.gender;
+
+    let error = {
+        "has_error": false,
+        "error_list": Array()
+    };
+    if (name == undefined) {
+        error.has_error = true;
+        error.error_list.push({
+            "missing_name": "You must provide a name for the user!"
+        })
+    }
+    if (email == undefined) {
+        error.has_error = true;
+        error.error_list.push({
+            "missing_email": "You must provide a email for the user!"
+        })
+    }
+    if (gender == undefined) {
+        error.has_error = true;
+        error.error_list.push({
+            "missing_gender": "You must provide a gender for the user!"
+        })
+    }
+
+    if (error.has_error) {
+        res.status(400).json(["Your made a bad request!", error.error_list]);
+    }
+    else {
+        res.json(createUser(name, email, gender));
+    }
 });
+
+app.get("/listUsers", (req, res) => {
+    res.json(listUsers())
+})
+
 
 export default app;
---
+++ src/models/user.ts
@@ -0,0 +1,68 @@
+import gender_list from "./gender_list.json";
+
+let user_list: any[] = [];
+let last_id = 0;
+
+
+class User {
+    static _id: number = last_id + 1;
+    #id: number;
+    name: string;
+    email: string;
+    gender: string;
+
+    public constructor(name: string, email: string, gender: string) {
+        this.#id = User._id++;
+        this.name = name;
+        this.email = email;
+        this.gender = "other";
+        for (let x of gender_list) {
+            if (x === gender)
+                this.gender = x;
+        }
+    }
+
+    public get info() {
+        return {
+            "id": this.#id,
+            "name": this.name,
+            "email": this.email,
+            "gender": this.gender,
+        };
+    }
+}
+
+const userExists = (name: string, email: string) => {
+    for (let user of user_list) {
+        if (user.name == name) {
+            return true;
+        }
+    }
+    for (let user of user_list) {
+        if (user.email == email) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+export const createUser = (name: string, email: string, gender: string) => {
+    email = email.toLowerCase();
+    if (!userExists(name, email)) {
+        gender = gender.toLowerCase();
+        let user = new User(name, email, gender);
+        user_list.push(user);
+        last_id++;
+        return user.info;
+    }
+    else {
+        return {
+            "user_already_exists": "This user name or user email is already registered!"
+        }
+    }
+}
+
+export const listUsers = () => {
+    return user_list
+}
